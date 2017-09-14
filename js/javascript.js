cd12e5cbcb9045921e500234d60c2b37d59d64cf
binmei <billbinmei@hotmail.com>
2017-09-14 16:33:41 -0700

    Implemented to Convert To UTC
    Finished implementing Converting to UTC method, will integrate with
    ServiceNow tomorrow.

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,18 @@
 var ctr = 0;
 var list;
 var items;
+var listOfDetails;
+
+function Detail(date, reason){
+    this.date = date;
+    this.reason = reason;
+    this.getDate = function(){
+        return this.date; 
+    };
+    this.getReason = function(){
+        return this.reason;
+    };
+}
 
 function add(){
     var li2 = document.createElement("li");
@@ -12,6 +24,8 @@ function add(){
 function parse(){
     var retArr = [];
     var retStr = "";
+    listOfDetails =[];
+    var i;
     
     list = document.getElementById('list');
     items = list.getElementsByTagName('li');
@@ -20,7 +34,9 @@ function parse(){
     for(i = 0; i < items.length; i++){
         var date = new Date(items[i].getElementsByTagName("input")[0].value);
         var description = items[i].getElementsByTagName("textarea")[0].value;
+        var detail = new Detail(date, description);
         retStr += "At " + date + " " + description + "\n";
+        listOfDetails.push(detail);
     }
     
     console.log(retStr);
@@ -28,19 +44,17 @@ function parse(){
 
 function convertTime(){
     var timeFormat = document.getElementsByTagName("select")[0].value;
-    if(timeFormat === 'Pacific'){
-        convertToPacific();
-    }
-    if(timeFormat === 'GMT'){
-        convertToUTC();
+    if(timeFormat.includes('Pacific')){
+        convertToUTC(listOfDetails);
     }
 }
 
-function convertToPacific(){
+function convertToUTC(listOfDetails){
+    var i;
     
-}
-
-function convertToUTC(){
-    
-    
+    for(i = 0; i < listOfDetails.length; i++){
+        listOfDetails[i].date = listOfDetails[i].getDate().toLocaleString('en-US', { timeZone:'Europe/London'});
+        console.log(detail.getDate());
+        console.log(detail.getReason());
+    }
 }
