168c5b7cb7f296e68fe0536c9f53bfa5eab9d574
binmei <billbinmei@hotmail.com>
2017-09-21 17:40:33 -0700

    Updated All Around
    Cleaning up codes and relying on array of Detail object instead of
    string.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,8 @@
-        var idCtr = 0;
-		var list;
-		var items;
+		var idCtr = 0;
 		var listOfDetails = [];
-		var utcStr = '';
-		
+		var snStr = "";
+        var retStr = "";
+                    
 		function Detail(date, reason){
 			this.date = date;
 			this.reason = reason;
@@ -16,58 +15,83 @@
 		}
 
 		function addRow(){
+            var elementID = "rca_li" + ++idCtr;
 			var newLi = document.createElement("li");
-            var newID = "li" + ++idCtr;
-			newLi.innerHTML = document.getElementById('li1').innerHTML;
-            newLi.setAttribute("class", "rca_element");
-            newLi.setAttribute("id", newID);
+			newLi.innerHTML = document.getElementById('rca_li0').innerHTML;
+            newLi.setAttribute("class", "rca_list_element");
+            newLi.setAttribute("id", elementID);            
 			document.getElementById('rca_list').append(newLi);
-			// idCtr++;
 		}
 
-		// function delete(this){
-			// var li = this.parentNode;
-			// li = null;
-		// }
-
-		function parse(){
-			var retArr = [];
-			var retStr = "";
-			var i;
-
-			list = document.getElementById('rca_list');
-			items = list.getElementsByTagName('li');
-
-
+		function deleteRow(element){
+			$(element).parent('li').remove();
+            parseInput();l
+		}
+        
+        function parseInput(){
+            listOfDetails = [];
+            
+            var i;
+            var list = document.getElementById('rca_list');
+            var items = list.getElementsByTagName('li');
+        
 			for(i = 0; i < items.length; i++){
 				var date = new Date(items[i].getElementsByTagName("input")[0].value);
 				var description = items[i].getElementsByTagName("textarea")[0].value;
 				var detail = new Detail(date, description);
-				retStr += "At " + date + " " + description + "\n";
 				listOfDetails.push(detail);
 			}
+        }
+        
+        function displayList(){
+            
+            var i;
+            var displayStr = '';
+            
+            for(i = 0; i < listOfDetails.length; i++){
+				var detail = listOfDetails[i];
+                displayStr += detail.getDate() + " " + detail.getReason() + "\n";
+			}
 
-			console.log(retStr);
+            document.getElementById('rca_preview_text').innerHTML = displayStr;
+        }
+        
+		function preview(){
+			parseInput();
+			displayList();
 		}
-
+        
+        function clear(){
+            document.getElementById('rca_preview_text').innerHTML = "";
+        }
+        
 		function convertTime(){
 			var timeFormat = document.getElementsByTagName("select")[0].value;
-			if(timeFormat.includes('PST') || timeFormat.includes('PDT') || timeFormat.includes('Pacific')){
+			if(timeFormat.includes('GMT')){
 				convertToUTC(listOfDetails);
 			}
+            clear();
+            displayList();
 		}
 
-		function convertToUTC(listOfDetails){
+		function convertToUTC(listOfDetails){            
 			var i;
+            var tempStr = "";
 		
 			for(i = 0; i < listOfDetails.length; i++){
 				var detail = listOfDetails[i];
-				detail.date = detail.getDate().toLocaleString('en-US', { timeZone:'Europe/London'});
-				console.log("At " + detail.getDate() + "(UTC)" + detail.getReason());
-				utcStr += "At " + detail.getDate() + "(UTC)" + detail.getReason() + "\n";
+                var weekDay = [7];
+                weekDay[1] = "Monday";
+                weekDay[2] = "Tuesday";
+                weekDay[3] = "Wednesday";
+                weekDay[4] = "Thursday";
+                weekDay[5] = "Friday";
+                weekDay[6] = "Saturday";
+                weekDay[7] = "Sunday";               
+                var dayOfWeek = weekDay[detail.getDate().getDay()];
+                
+				detail.date = dayOfWeek + ", " + detail.getDate().toLocaleString('en-US', { timeZone:'Europe/London'}) + " (UTC)";
 			}
-			console.log("in convertToUTC function: " + utcStr);
-
 		}
 		
 		function submitToSN(str){
@@ -76,9 +100,9 @@
 			}
 		
 			var xhr = new XMLHttpRequest();
-			xhr.open("POST", 'https://requestb.in/1iz9vsi1', true);
-			// <!--xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
-			// xhr.setRequestHeader('Access-Control-Allow-Origin', '*'); -->
+//<!-- 			xhr.open("POST", 'https://theplatformdev.service-now.com/rca_compiler.do', true); -->
+			xhr.open("POST", 'https://requestb.in/xmwytaxm', true);
+
 			xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 			xhr.send(JSON.stringify(payload));
 			console.log(JSON.stringify(payload));
